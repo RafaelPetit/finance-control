b5f6dd9af1e4eb6586578b082cfb7e99f6bcfaab
Rafael Petit <rpetit.dev@gmail.com>
2024-07-21 19:46:29 -0300

    feat: Tela de signUp

---
+++ finance-control-ui/app/signUp/page.tsx
@@ -0,0 +1,106 @@
+"use client"
+import React, { useState } from 'react';
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleChange = (e: { target: { name: any; value: any; }; }) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      setPasswordError('As senhas não coincidem');
+      return;
+    }
+    setPasswordError('');
+    console.log('Cadastro realizado:', formData);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-xl">
+        <h1 className="text-2xl font-bold mb-6 text-center">Sign Up</h1>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="name" className="text-gray-600">Nome</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Nome"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="email" className="text-gray-600">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="password" className="text-gray-600">Senha</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Senha"
+              value={formData.password}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="confirmPassword" className="text-gray-600">Confirmar Senha</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              placeholder="Confirmar Senha"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+          {passwordError && (
+            <p className="text-red-500 text-sm">{passwordError}</p>
+          )}
+          <button
+            type="submit"
+            className="w-full bg-gradient-to-r from-blue-500 to-purple-500 text-white py-3 rounded-lg hover:from-purple-500 hover:to-blue-500 transition-colors duration-300"
+          >
+            Sign Up
+          </button>
+          <p className="text-center">
+            Already have an account? <a href="/auth/login" className="text-blue-500 hover:underline">Login</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
