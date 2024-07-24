e3add21d67f7970d2504f631582d1d6a6fbc6292
Rafael Petit <rpetit.dev@gmail.com>
2024-07-24 13:57:19 -0300

    refactor: Migrar para MySql

---
+++ financial-control-backend/prisma/migrations/20240724165332_migrate_to_my_sql/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `photo` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Income` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `description` VARCHAR(191) NOT NULL,
+    `amount` DOUBLE NOT NULL,
+    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `category` ENUM('SALARY', 'GIFT', 'SALE', 'INVESTMENT', 'RENTAL', 'PRIZE', 'OTHER') NOT NULL,
+    `status` ENUM('ACTIVE', 'INACTIVE') NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Expense` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `description` VARCHAR(191) NOT NULL,
+    `amount` DOUBLE NOT NULL,
+    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `category` ENUM('FOOD', 'TRANSPORT', 'HOUSING', 'HEALTH', 'EDUCATION', 'LEISURE', 'CLOTHING', 'COMMUNICATIONS', 'SERVICES', 'TAXES', 'DONATIONS', 'INVESTMENTS', 'INSURANCE', 'OTHER') NOT NULL,
+    `paymentMethod` ENUM('CREDIT', 'DEBIT') NOT NULL,
+    `status` ENUM('ACTIVE', 'INACTIVE') NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Income` ADD CONSTRAINT `Income_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Expense` ADD CONSTRAINT `Expense_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
